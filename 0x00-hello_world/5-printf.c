346b9db34dd0a8943d91227f771b06bc25d62e5f
Chemuu-05 <chemuunaomy@gmail.com>
2023-03-16 14:22:30 -0700

    printf.c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * This is a program that prints a new line using printf function
+ * Return: Always 0 and shows sucess
+ */
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
