0d4ed2d65385fba166e09526213dea69e6b1a776
Chemuu-05 <chemuunaomy@gmail.com>
2023-03-16 14:09:29 -0700

    4-puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * This is a program that prints a new line using puts function
+ * Return: It's value is 0 and shows end of file or errors
+ */
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
