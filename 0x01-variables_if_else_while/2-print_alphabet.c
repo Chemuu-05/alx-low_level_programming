ea0fdc9241fdfadd0ef4aabd5579b04ca6378196
Chemuu-05 <chemuunaomy@gmail.com>
2023-03-17 13:24:53 -0700

    2-print_alphabet.c file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * prints the alphabet in lowercase
+ * Return: value 0 (sucess)
+ */
+
+int main(void)
+{
+	char c;
+
+	for(c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
