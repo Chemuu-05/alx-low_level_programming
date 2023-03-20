956ad29c15e3ac8797c708e7a303159237b4d06c
Chemuu-05 <chemuunaomy@gmail.com>
2023-03-20 12:40:35 -0700

    quote.c file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-
 #include <unistd.h>
 
 /**
  * main - Entry point
  * prints a line to the standard error
- * Return: 1
+ * Return: 1 sucess
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\", - Dora Korpar, 2015-10-19\n", 61);
+	write(2,
+	      "and that piece of art is useful\", - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
