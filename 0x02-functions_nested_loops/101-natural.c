bbaab769ee6b26dc54450924112f2aa8b1f2877f
Chemuu-05 <chemuunaomy@gmail.com>
2023-03-22 05:45:41 -0700

    101-natural.c file

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entr point
+ * prints the sum of all the multiples of 3 or 5 below 1024
+ * Return: value is 0
+ */
+int main(void)
+{
+	int i, sum;
+
+	sum = 0
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * fibonum - prints the first 98 Fibonacci numbers
+ * @n: operation number
+ * Return: number of digits
+ */
+int fibonum(int n)
+{
+	int num = 0;
+	if (
