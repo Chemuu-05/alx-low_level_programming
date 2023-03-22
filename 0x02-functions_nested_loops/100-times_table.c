1d59f5b03992a5b88e07a9c1dc9f5211bf7f0266
Chemuu-05 <chemuunaomy@gmail.com>
2023-03-22 06:38:23 -0700

    24_hours.c file

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *print_times_table - prints the n times table
+ *@n: The number to be printed
+ * Return: nothing
+ */
+void print_times_table(int n)
+{
+	int n;
+	{
+		if (n = 0; n > 15; n < 15 n++)
+		{
+			_putchar
