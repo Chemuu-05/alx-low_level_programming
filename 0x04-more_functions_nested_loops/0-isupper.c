37a0f00db24ce9103ea69309b6f29b59b231f64e
Chemuu-05 <chemuunaomy@gmail.com>
2023-03-23 22:04:55 -0700

    1-isdigit.c file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,13 +6,17 @@
  *Return: 1 if c is uppercase and 0 otherwise
  */
 
-int _isupper(int c)
+int _isupper(int c;)
 {
 	int c;
 
 	if
 		(c => 'A' && c <= 'Z')
-			return (1);
+	{
+		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
