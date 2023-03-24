6acd09fa07b80f2bc13e0f3ec926fdc57503a61e
Chemuu-05 <chemuunaomy@gmail.com>
2023-03-24 02:35:35 -0700

    print_number.c file

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,19 +9,19 @@
  */
 void print_number(int n)
 {
-	unsigned int a = n;
+	unsigned int k = n;
 
 	if (n < 0)
 	{
 		n *= -1;
-		a = n;
-		_putchar('_');
+		k = n;
+		_putchar('-');
 	}
+	
+	k /= 10;
 
-	a /= 10;
-
-	if (a != 0)
-		print_number(a);
+	if (k != 0)
+		print_number(k);
 
 	_putchar((unsigned int) n % 10 + '0');
 }
