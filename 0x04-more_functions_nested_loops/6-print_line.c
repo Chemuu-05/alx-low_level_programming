de621139158e456f092504ab7c93f91892630aa7
Chemuu-05 <chemuunaomy@gmail.com>
2023-03-24 13:06:46 -0700

    print_line.c file

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -15,11 +15,11 @@ void print_line(int n)
 	}
 	else
 	{
-		int n;
-		
-		for (n = 1; n++;)
+		int x;
+
+		for (x = 0; x < n; x++)
 		{
-			_putchar('_');
+			_putchar(95);
 		}
 		_putchar('\n');
 	}
