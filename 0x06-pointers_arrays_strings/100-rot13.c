78c87b5b02b1db639251d2614a571926899d7149
Chemuu-05 <chemuunaomy@gmail.com>
2023-03-30 04:27:49 -0700

    rot13.c file

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,11 +1,33 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * *rot13 - encodes a string using rot13
+ * @n: string to be encoded
+ * Return: encoded string
  */
 
 char *rot13(char *n)
 {
+	int a = 0;
+
+	while (n[a])
+	{
+		while ((n[a] >= 'a' && n[a] <= 'z') || (n[a] >= 'A' && n[a] <= 'Z'))
+		{
+			if ((n[a] > 'm' && n[a] <= 'z') || (n[a] > 'M' && n[a] <= 'Z'))
+			{
+				n[a] -= 13;
+				break;
+			}
+
+			n[a] += 13;
+			break;
+		}
+
+		a++;
+	}
+
+	return (n);
+}
+
 
