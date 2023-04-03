a6557f584cb2e7d3ff62e50172112499a2adef96
Chemuu-05 <chemuunaomy@gmail.com>
2023-04-03 03:05:43 -0700

    memset.c file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @s: the string to check
+ * @c: character to be located
+ * Return: NULL if the character is not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	for (s = 0; s != c; s++)
+	{
+		s++;
+	}
+	if
+	{
+		return (s);
+	}
+	else (c == '\0')
+		return (s);
+
+	return (NULL);
+}
