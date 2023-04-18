6d1fb7f40f3de0e03b03edd77d4e1e7967ffedab
Chemuu-05 <chemuunaomy@gmail.com>
2023-04-18 07:28:54 -0700

    gog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,9 +6,9 @@
   * @name: The name of the dog.
   * @age: The age of the dog.
   * @owner: The owner of the dog.
+  *
   * Description: The attributes of a dog.
   */
-
 struct dog
 {
 	char *name;
@@ -17,7 +17,7 @@ struct dog
 };
 
 /**
- * dog_t - Typedef for struct dog
+ * dog_t - Typedef for dog structure
  */
 typedef struct dog dog_t;
 
@@ -25,7 +25,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
-
 #endif
