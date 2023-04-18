7ab503564dce313094abaaac59a4a68e4accd2eb
Chemuu-05 <chemuunaomy@gmail.com>
2023-04-18 00:57:05 -0700

    main.c file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#ifndef _HEADER_
+#define _HEADER_
+#define TEXT "Hello, Holberton"
+#endif
+#include <stdio.h>
+
+/**
+  * main - prints the name of the file it was compiled from.
+  * Return: nothing
+  */
+
+int main(void)
+{
+	printf("%s\n", TEXT);
+	return (0);
+}
