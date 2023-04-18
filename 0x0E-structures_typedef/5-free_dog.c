cade06d39b3c9bfd5e16f0c7ac7c2d0f68cabcca
Chemuu-05 <chemuunaomy@gmail.com>
2023-04-18 07:23:19 -0700

    free_dog.c file

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+  * free_dog - frees dogs.
+  * @d: pointer to the dog to be freed
+  * Return: nothing
+  */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
