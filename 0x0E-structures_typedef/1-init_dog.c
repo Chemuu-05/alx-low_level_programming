dfe63a534d9d056f3692d607e8311c7257204339
Chemuu-05 <chemuunaomy@gmail.com>
2023-04-18 06:31:24 -0700

    1-init_dog.c file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,7 +3,7 @@
 
 /**
   * init_dog - Initializes a dog structure
-  * @d: A dog structure.
+  * @d: pointer to struct dog to initialize.
   * @name: The name of the dog.
   * @age: The age of the dog.
   * @owner: The owner of the dog.
@@ -12,7 +12,8 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
 	{
 		d->name = name;
 		d->age = age;
