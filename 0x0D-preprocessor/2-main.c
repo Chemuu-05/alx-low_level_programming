8d87617c11df03d7d4b39038ff6853753b9a3efa
Chemuu-05 <chemuunaomy@gmail.com>
2023-04-18 01:14:41 -0700

    main.c file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,16 +1,12 @@
-#ifndef _HEADER_
-#define _HEADER_
-#define TEXT "Hello, main"
-#endif
 #include <stdio.h>
 
 /**
-  * main - prints the name of the file it was compiled from.
-  * Return: nothing
+  * main - Prints the file name of the program
+  * Return: Always zero
   */
 
 int main(void)
 {
-	printf("%s\n", TEXT);
+	printf("%s\n", __FILE__);
 	return (0);
 }
